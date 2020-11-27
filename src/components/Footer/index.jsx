ab7f1a09bb9ea1ae28361fc0f6a3a55b4eed4f72
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-26 23:44:00 -0800

    footer styling

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,10 +9,34 @@ class Footer extends Component {
                     TOPPPY
                 </div>
                 <div className="footer_bottom">
+                    <div className="footer_box">
+                        <div className="footer_icons">
+                            <span>X</span>
+                            <span>X</span>
+                            <span>X</span>
+                            <span>X</span>
+                            <span>X</span>
+                            <span>X</span>
+                        </div>
+                    </div>
                     <div className="footer_box">THING</div>
                     <div className="footer_box">THING</div>
-                    <div className="footer_box">THING</div>
-                    <div className="footer_box">THING</div>
+                    <div className="footer_box">
+                        <ul>
+                            <li>
+                                <a href="/">THING</a>
+                            </li>
+                            <li>
+                                <a href="/">THING</a>
+                            </li>
+                            <li>
+                                <a href="/">THING</a>
+                            </li>
+                            <li>
+                                <a href="/">THING</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
 
             </section>
