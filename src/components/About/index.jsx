f1de5afdcc5d5be28b67cea4daba42ca8bc99182
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-30 18:19:56 -0800

    color palette/scss vars

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,13 +5,12 @@ import hammockImg from './images/hammock.png';
 class Hero extends Component {
     render() {
         return (
-            <section className="about_container">
-                <div className="about_card">
-                    <div className="image_container">
-                        <h3 className="sm_header">hangin' free since '83</h3>
-                        <img className="about_image" src={hammockImg} alt="" />
-                    </div>
-                    <div className="text_container">
+            <section className="about_card">
+                <div className="image_container">
+                    <h3 className="sm_header">hangin' free since '83</h3>
+                    <img className="about_image" src={hammockImg} alt="" />
+                </div>
+                <div className="text_container">
                     <h1>ABOUT US</h1>
                     <hr />
                     <p className="about_copy">
@@ -32,7 +31,6 @@ class Hero extends Component {
                         occaecat cupidatat non proident, sunt in culpa qui officia
                         deserunt mollit anim id est laborum.
                 </p>
-                </div>
                 </div>
             </section>
         )
